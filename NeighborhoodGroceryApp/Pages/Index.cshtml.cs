57fec890737415920ad2b058c93fcedd312135e3
Parth Doshi <doshipj@mail.uc.edu>
2019-11-18 15:11:41 -0500

    CodeReviewAssignment- doshipj

--- NeighborhoodGroceryApp/Pages/Index.cshtml.cs
+++ NeighborhoodGroceryApp/Pages/Index.cshtml.cs
@@ -11,26 +11,24 @@ namespace NeighborhoodGroceryApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public JsonResult OnGet()
         {
             string housingJson = GetData("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
             HousingModel[] housingData = HousingModel.FromJson(housingJson);
-
+            ViewData["housingData"] = housingData;
 
             string groceryJson = GetData("https://data.cityofchicago.org/resource/ce29-twzt.json");
-            //var v = JsonConvert.DeserializeObject<GroceryStoreModel[]>(groceryJson);
             GroceryStoreModel[] groceryData = GroceryStoreModel.FromJson(groceryJson);
+            ViewData["groceryData"] = groceryData;
 
-
+            return new JsonResult(groceryData);
         }
         public string GetData(string endpoint)
         {
-            string downloadedData = "";
             using (WebClient webClient = new WebClient())
             {
-                downloadedData = webClient.DownloadString(endpoint);
+                return webClient.DownloadString(endpoint);
             }
-            return downloadedData;
         }
     }
 }
