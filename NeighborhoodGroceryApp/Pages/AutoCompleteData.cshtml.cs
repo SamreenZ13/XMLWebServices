eb9f2279fc8fcc9661724dc59c3226def1f5e0b8
SamreenZ13 <54732954+SamreenZ13@users.noreply.github.com>
2019-12-02 17:13:44 -0500

    modified autocomplete changes

---
+++ NeighborhoodGroceryApp/Pages/AutoCompleteData.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GroceryStore;
+using Housing;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+
+namespace NeighborhoodGroceryApp.Pages
+{
+    public class AutoCompleteDataModel : PageModel
+    {
+        [BindProperty]
+        private string Term { get; set; }
+        private IList<string> communityList = new List<string>();
+        public JsonResult OnGet()
+        {
+            Term = HttpContext.Request.Query["Term"];
+            string housingJson = GetData("https://data.cityofchicago.org/resource/s6ha-ppgi.json");
+            HousingModel[] housingData = HousingModel.FromJson(housingJson);
+
+            //get the community list data for autopopulate
+            communityList = housingData.Where(i => !string.IsNullOrEmpty(i.CommunityArea) && i.CommunityArea.ToLower().Contains(Term.ToLower()))
+                                       .Select(i => i.CommunityArea).Distinct().ToList();
+            return new JsonResult(communityList);
+        }
+        //GetData method is created to read the data from two JSONs. A single method, so that it can be used.
+        public string GetData(string endpoint)
+        {
+            string downloadedData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedData = webClient.DownloadString(endpoint);
+            }
+            return downloadedData;
+        }
+    }
+}
