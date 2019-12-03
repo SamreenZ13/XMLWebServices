94be65d1faecd040702e0a5c9f24c9a605d544ba
SamreenZ13 <54732954+SamreenZ13@users.noreply.github.com>
2019-12-02 19:44:09 -0500

    consuming fellow student API

---
+++ NeighborhoodGroceryApp/Pages/ArtistSongs.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NeighborhoodGroceryApp.Pages
+{
+    public class ArtistSongsModel : PageModel
+    {
+        public ActionResult OnGet()
+        {
+            string artistString = GetData("https://concertfit20191109074437.azurewebsites.net/api");
+            Empty[] artistJSON = Empty.FromJson(artistString);
+            ViewData["artistdata"] = artistJSON.ToList();
+            return Page();
+        }
+
+        public string GetData(string endpoint)
+        {
+            string downloadedData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedData = webClient.DownloadString(endpoint);
+            }
+            return downloadedData;
+        }
+    }
+}
